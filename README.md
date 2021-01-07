c87cf274a83f731015251e1ec7a20fe8b2f23f25
bsao17 <declic62@gmail.com>
2021-01-07 15:24:48 +0100

    Init before heroku deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+I prefered recreate api entirely with a new database instead of working with a project that alread existed, it'better and more clean.
+I work again with APIplatform and now,  my symfony code is more clear !
---
+++ src/Entity/Comment.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ApiResource (
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"},
+ *     normalizationContext={
+            "groups"={"comment-read"}
+ *     }
+ * )
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups ({"comment-read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Groups ({"comment-read"})
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Billet::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups ({"comment-read"})
+     */
+    private $billet;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups ({"comment-read"})
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getBillet(): ?Billet
+    {
+        return $this->billet;
+    }
+
+    public function setBillet(?Billet $billet): self
+    {
+        $this->billet = $billet;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
