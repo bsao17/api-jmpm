fef81e224563b69210468ddffa5584a6316ebcab
bsao17 <declic62@gmail.com>
2021-02-21 21:25:24 +0100

    use BilletRepository in update page

--- src/Controller/IsUpToDateController.php
+++ src/Controller/IsUpToDateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BilletRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,13 @@ class IsUpToDateController extends AbstractController
 {
     /**
      * @Route("/isupdate", name="is_up_to_date")
+     * @param BilletRepository $billetRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(BilletRepository $billetRepository): Response
     {
         return $this->render('is_up_to_date/index.html.twig', [
-            'controller_name' => 'IsUpToDateController',
+            'billets' => $billetRepository->findAll()
         ]);
     }
 }
