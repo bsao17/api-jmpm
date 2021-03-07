d802042e4c72324d19460e924e570e3fdffc6e03
bsao17 <declic62@gmail.com>
2021-03-07 13:03:32 +0100

    Update Billet CRUD

---
+++ src/Controller/DocumentationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DocumentationController extends AbstractController
+{
+    /**
+     * @Route("/documentation", name="documentation")
+     */
+    public function index(): Response
+    {
+        return $this->render('documentation/index.html.twig', [
+            'controller_name' => 'DocumentationController',
+        ]);
+    }
+}
